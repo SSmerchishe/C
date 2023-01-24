5af00260912f5f4b27f47772d52c037c3c1c749a
SmerchisheW <smerchishe@gmail.com>
2023-01-24 18:55:30 +0300

    lesson1\sum

---
+++ Lesson 1/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int a, b, sum;
+    printf("Enter two numbers:\n");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("%d + %d = %d", a, b, sum);
+    return 0;
+}
