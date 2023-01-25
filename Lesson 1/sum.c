6154711b6f20506f1291a1d9ddf3307109fefbed
SmerchisheW <smerchishe@gmail.com>
2023-01-25 11:53:15 +0300

    edit output message in sum.c

--- Lesson 1/sum.c
+++ Lesson 1/sum.c
@@ -6,6 +6,6 @@ int main(int argc, char* argv[])
     printf("Enter two numbers:\n");
     scanf("%d%d", &a, &b);
     sum = a + b;
-    printf("%d + %d = %d <<<< look man", a, b, sum);
+    printf("%d + %d = %d <<<< my result", a, b, sum);
     return 0;
 }
